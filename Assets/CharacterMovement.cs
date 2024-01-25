80ae28546c929eaaf722ba5121a0f4ec099d6d53
Đinh Nhật Hoàng <135942508+hoaq139@users.noreply.github.com>
2024-01-25 17:15:20 +0700

    Hoaq scene

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    private BoxCollider2D coll;
+    private SpriteRenderer sprite;
+    private Animator anim;
+    private float dirX = 0f;
+    [SerializeField] private LayerMask jumpableGround;
+    [SerializeField] public float jumpForce = 14f;
+    [SerializeField] public float moveSpeed = 7f;
+    private enum MovementState
+    {
+        idle, running, jumping, falling
+    }
+    private MovementState state = MovementState.idle;
+    private int remainingJumps = 0; // Adjust this to the desired number of jumps
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+        // Check if the character is grounded
+        if (IsGrounded())
+        {
+            remainingJumps = 1; // Reset the remaining jumps when grounded
+        }
+
+        // Xử lý nhảy chỉ khi đối tượng đang nằm trên mặt đất và có lượt nhảy còn lại
+        if (Input.GetButtonDown("Jump") && remainingJumps > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            remainingJumps--;
+        }
+
+        UpdateAnimationUpdate();
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down,.1f,jumpableGround);
+    }
+
+    private void UpdateAnimationUpdate()
+    {
+        MovementState state;
+        if (dirX > 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = true;
+        }
+        else
+        {
+            state = MovementState.idle;
+        }
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+        anim.SetInteger("state", (int)state);
+    }
+}
