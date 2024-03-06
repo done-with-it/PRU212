22e288f8ffa40af92eb9b615c8b20847c7fa3277
Dante_DEV <110292815+done-with-it@users.noreply.github.com>
2024-03-06 17:55:08 +0700

    update code

--- Assets/Scripts/Hoaq scene/CharacterMovement.cs
+++ Assets/Scripts/Hoaq scene/CharacterMovement.cs
@@ -55,7 +55,6 @@ public class CharacterMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             remainingJumps--;
         }
-
         UpdateAnimationUpdate();
     }
     private bool IsGrounded()
